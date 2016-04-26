9a77ad1a8f4b465f581f9b9e3ffda2d5f4d93493
na57 <na57@qq.com>
2016-04-26 10:57:53 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 dt=$(date +%Y%m%d)
-./consul-backup -i $CONSUL_ENDPOINT -t $CONSUL_TOKEN -a -b acl.bak.$dt kv.bak.$dt
+./consul-backup -i $CONSUL_ENDPOINT -t $CONSUL_TOKEN -a -b /backup/acl.bak.$dt /backup/kv.bak.$dt
 sleep 25h
 ./entry.sh
